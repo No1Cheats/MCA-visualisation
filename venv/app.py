71221327bdac5d669f5526e70dc2fbc20155e8a1
No1Cheats <jan.imhof@gmail.com>
2020-02-27 16:21:50 +0100

    Added part of the visualisation

--- venv/app.py
+++ venv/app.py
@@ -1,13 +1,15 @@
-#Monte-Carlo Algorithm visualisation
+# Monte-Carlo Algorithm visualisation
 
-#Imports:
+# Imports:
 import os
 import random
 import numpy as np
+import pandas as pd
+from matplotlib import pyplot as plt
 
-#Funcations:
+
+# Functions:
 def calc_pi(points):
-
     counter = 0
     total_points = points
     ratio = 0
@@ -15,10 +17,10 @@ def calc_pi(points):
     y = 0
     pi = 0
 
-    for i in range(0,total_points):
+    for i in range(0, total_points):
         x = random.random()
         y = random.random()
-        if(x**2 + y**2 <= 1):
+        if x ** 2 + y ** 2 <= 1:
             counter += 1
             ratio = counter / total_points
 
@@ -27,22 +29,24 @@ def calc_pi(points):
     return pi
 
 
-
-def visualisze(amount_of_iterations):
+def get_csv(amount_of_iterations):
     results = []
-    for i in range (1,amount_of_iterations):
+    for i in range(1, amount_of_iterations):
         results.append(calc_pi(i))
 
-    np.savetxt('data.csv', (results), delimiter=',')
-
-visualisze(1000)
+    np.savetxt('data100k.csv', results, delimiter=',')
 
 
+def visualize():
+    my_data = pd.read_csv('data.csv')
+    plt.plot(my_data)
+    pi = plt.axhline(y=3.14159)
+    pi.set_color('red')
+    plt.show()
 
 
-
-
-
+# get_csv(100000)
+visualize()
 
 '''
 
