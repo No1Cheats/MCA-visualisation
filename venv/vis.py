fa6559f54d124f294f3f57aadceabe3d184b5840
No1Cheats <jan.imhof@gmail.com>
2020-02-29 22:28:33 +0100

    Added visualisation

--- venv/vis.py
+++ venv/vis.py
@@ -2,31 +2,43 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 import matplotlib.animation as animation
+import csv
 
-#Writer = animation.writers['ffmpeg']
-#writer = Writer(fps=15, metadata=dict(artist='Me'), bitrate=1800)
+Writer = animation.writers['ffmpeg']
+writer = Writer(fps=15, metadata=dict(artist='Me'), bitrate=1800)
 
 
 fig, ax = plt.subplots()
 xdata, ydata = [], []
-ln, = plt.plot([], [], 'ro')
-my_list = [4, 3, 4, 3.4, 3, 3.1]
+ln, = plt.plot([], [], '-')
+
+with open('data1k.csv', newline='') as f:
+    reader = csv.reader(f)
+    my_data = list(reader)
+
+
+def convert(frame):
+    y_value = my_data[int(frame)][0]
+    return float(y_value[:20])
 
 
 def init():
-    ax.set_xlim(0, 10)
-    ax.set_ylim(2, 6)
+    ax.set_xlim(0, 1000)
+    ax.set_ylim(2, 4.5)
+    pi = plt.axhline(y=3.14159)
+    pi.set_color('red')
     return ln,
 
 
 def update(frame):
     xdata.append(frame)
-    ydata.append(my_list[frame])
+    ydata.append(convert(frame))
     ln.set_data(xdata, ydata)
     return ln,
 
 
-ani = FuncAnimation(fig, update, frames=5, init_func=init, blit=True)
+ani = FuncAnimation(fig, update, frames=990,
+                    init_func=init, blit=True)
 plt.show()
 
-#ani.save('vis.mp4', writer=writer)
+ani.save('vis.mp4', writer=writer)
